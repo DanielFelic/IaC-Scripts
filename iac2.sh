27c72d6fb80f35b8f534fab031b3032fecaeb5ea
DanielFelic <danielfeliciano.dev@gmail.com>
2023-01-08 14:13:55 +0000

    Script provisionamento de servidor Apache

---
+++ iac2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Atualizando sistema..."
+
+apt-get update
+apt-get upgrade -y
+
+echo "Instalando programas..."
+
+apt-get install apache2 -y
+apt-get install unzip -y
+
+echo "Baixando aplicação..."
+
+cd /tmp
+wget https://github.com/DanielFelic/Web-Projects.git
+
+echo "Copiando aplicação para diretório Apache..."
+
+unzip Web-Projects-master.zip
+cd /tmp/Web-Projects-master/minimalist-website-2022
+cp -R * /var/www/html/
